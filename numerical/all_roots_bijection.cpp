acba2cb933797c13d54e0e739f72fdaf408fb0cc
ashura <fmn.flame@gmail.com>
2026-02-22 01:07:27 +0600

    online 3 numerical

--- numerical/all_roots_bijection.cpp
+++ numerical/all_roots_bijection.cpp
@@ -1,59 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 double f(double x)
 {
-    return x*x*x-x-2;
+    return x*x*x - 6*x*x + 11*x - 6; // x^3 - 6x^2 + 11x - 6
 }
 
 double bisection(double x1, double x2, double eps)
 {
     double previousRoot = x1;
-    double error =1e9;
+    double error = 1e9;
     double root;
 
-    while(error >= eps)
+    while (error >= eps)
     {
-        root=(x1+x2)/2.0;
-        error = fabs(root-previousRoot);
+        root = (x1 + x2) / 2.0;
+        error = fabs(root - previousRoot);
 
-        if(f(root)==0.0)
+        if (f(root) == 0.0)
             break;
-        else if(f(root)*f(x1)<0)
-            x2=root;
-        else    
-            x1=root;
-        
-        previousRoot=root;
+        else if (f(root) * f(x1) < 0)
+            x2 = root;
+        else
+            x1 = root;
+
+        previousRoot = root;
     }
 
     return root;
-} 
-
+}
 
 void findroots(double a, double b, double ds, double eps)
 {
-    double x1=a;
-    double x2=x1+ds;
+    double x1 = a;
+    double x2 = x1 + ds;
 
-    while(x2<=b)
+    while (x2 <= b)
     {
-        if(f(x1)*f(x2)<0)
+        if (f(x1) * f(x2) < 0)
         {
             double root = bisection(x1, x2, eps);
-            cout<<"root found: "<<root<<endl;
+            cout << "Root found: " << root << endl;
         }
-        x1=x2;
-        x2=x1+ds;
+        x1 = x2;
+        x2 = x1 + ds;
     }
 }
+
 int main()
 {
-    double a, b, ds, eps;
-
-    cin >> a >> b;
-    cin >> ds;
-    cin >> eps;
+    // Predefined values
+    double a = 0, b = 4;    // search interval
+    double ds = 0.5;        // step to scan for sign changes
+    double eps = 1e-12;     // bisection tolerance
 
     findroots(a, b, ds, eps);
 
